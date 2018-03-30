7d8ef85a31946c0cdc22e1e20ccc165ead1d2e44
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-30 15:20:20 +0800

    15:20/03/30/18

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -9,20 +9,20 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
-        title = response.xpath('//div[@class["entry-header"]/h1/text()')
-        create_date = re.sub(
+        title = response.css('.entry-header h1::text').extract()[0]
+        post_date = re.sub(
             r'[ \r\n·]', '',
-            response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0]
+            response.css('.entry-meta-hide-on-mobile::text').extract()[0]
         )
         tag = ','.join(list(filter(
             lambda s: not s.rstrip().endswith('评论'),
-            response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
+            response.css('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
         )))
         content = response.xpath('//div[@class="entry"]').extract()[0]
-        praise_num = int(response.xpath('//span[contains(@class, "vote-post-up")]/h10/text()').extract()[0])
+        vote_num = int(response.css('.vote-post-up h10::text').extract()[0])
 
-        fav_num = response.xpath('//span[contains(@class, "bookmark-btn")]/text()').extract()[0]
-        match_re = re.match(r'.*(\d+).*', fav_num)
+        fav_num = response.css('.bookmark-btn::text').extract()[0]
+        match_re = re.match(r'.*?(\d+).*', fav_num)
         if match_re:
             fav_num = match_re.group(1)
 
