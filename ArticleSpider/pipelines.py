56a4054719366d02d3a0300b569f2f6cac538494
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-31 18:12:19 +0800

    18:12/03/31/18

--- ArticleSpider/pipelines.py
+++ ArticleSpider/pipelines.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import psycopg2
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exporters import JsonItemExporter
 
@@ -9,8 +10,12 @@ from scrapy.exporters import JsonItemExporter
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ArticlespiderPipeline(object):
-    def process_item(self, item, spider):
+class ArticleImgPipeline(ImagesPipeline):
+    def item_completed(self, results, item, info):
+        for ok, d in results:
+            if ok:
+                item['front_img_path'] = d['path']
+
         return item
 
 
@@ -30,10 +35,12 @@ class JsonExporterPipeline:
         self.file.close()
 
 
-class ArticleImgPipeline(ImagesPipeline):
-    def item_completed(self, results, item, info):
-        for ok, d in results:
-            if ok:
-                item['front_img_path'] = d['path']
-
-        return item
+class PostgreSQLPipeLine:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host='arch-linux',
+            port='5432',
+            user='aurevoirxavier',
+            password='',
+            database='jobbole_spider'
+        )
