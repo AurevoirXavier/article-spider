ad8e3d1987b24daf2c7fb43e6c0735b45f25fe9b
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-10 16:11:00 +0800

    16:11/04/10/18

--- StupidSpider/util/lagou_sign_in.py
+++ StupidSpider/util/lagou_sign_in.py
@@ -3,6 +3,7 @@ import requests
 
 from PIL import Image
 from time import time
+from hashlib import md5
 from parsel import Selector
 
 from StupidSpider.util.secret.secret import LAGOU_USERNAME, LAGOU_PASSWORD
@@ -10,14 +11,12 @@ from StupidSpider.util.secret.secret import LAGOU_USERNAME, LAGOU_PASSWORD
 SIGN_IN_PAGE = 'https://passport.lagou.com/login/login.html'
 SIGN_IN_API = 'https://passport.lagou.com/login/login.json'
 AUTH_API = 'https://passport.lagou.com/vcode/create?from=login&refresh={}'
-REFERER = 'https://passport.lagou.com/login/login.html?ts={}&serviceId=lagou&service=https%253A%252F%252Fwww.lagou.com%252F&action=login&signature=3DD28DEA42F297A5EDFCFDF1A962AE87C1'
 HEADERS = {
     'Connection': 'keep-alive',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15'
 }
 REQUEST_DATA = {
     'username': LAGOU_USERNAME,
-    'password': LAGOU_PASSWORD,
     'request_form_verifyCode': ''
 }
 
@@ -26,7 +25,6 @@ class LagouUser:
     def __init__(self):
         self.__sign_in_page = SIGN_IN_PAGE
         self.__sign_in_api = SIGN_IN_API
-        self.__referer = REFERER
         self.__auth_api = AUTH_API
         self.__request_data = REQUEST_DATA.copy()
         self.session = requests.session()
@@ -42,17 +40,17 @@ class LagouUser:
             ).css('head script:nth-last-child(2)::text').extract_first()
         )
 
-        timestamp = int(time() * 1000)
         headers = self.session.headers.copy()
         headers.update({
-            'Referer': self.__referer.format(timestamp),
+            'Referer': self.__sign_in_page,
             'X-Anit-Forge-Token': next(tokens).group(1),
             'X-Anit-Forge-Code': next(tokens).group(1)
         })
 
         if captcha:
             with open('captcha', 'wb') as f:
-                f.write(self.session.get(self.__auth_api.format(timestamp), headers=self.session.headers).content)
+                f.write(
+                    self.session.get(self.__auth_api.format(int(time() * 1000)), headers=self.session.headers).content)
 
             Image.open('captcha').show()
 
@@ -61,6 +59,14 @@ class LagouUser:
             self.__request_data.update({
                 'request_form_verifyCode': captcha
             })
+
+        self.__request_data.update({
+            'password': md5(
+                (
+                        'veenike' + md5(LAGOU_PASSWORD.encode('utf8')).hexdigest() + 'veenike'
+                ).encode('utf8')
+            ).hexdigest()
+        })
 
         debug_online_status = self.session.post(
             self.__sign_in_api,
@@ -82,8 +88,3 @@ class LagouUser:
 
 user = LagouUser()
 user.sign_in()
-
-debug_online_status = user.session.get('https://www.lagou.com/jobs/4385349.html')
-# test = requests.get('https://www.lagou.com/jobs/4385349.html', headers=HEADERS)
-
-pass
